370ced7f810fe941e7c6fc3c9e835c3edd809a2c
AGPress-Tech <tech@agpress-srl.it>
2025-03-07 11:25:53 +0100

    Added Shipping Schedule

--- scripts/programmi-scripts.js
+++ scripts/programmi-scripts.js
@@ -11,9 +11,10 @@ const filePaths = [
     "\\\\Dl360\\condivisa\\Programmi Reparti AGPress\\4 - Programma Tranceria.xlsx",
     "\\\\Dl360\\condivisa\\Programmi Reparti AGPress\\5 - Programma Torneria.xlsx",
     "\\\\Dl360\\condivisa\\Programmi Reparti AGPress\\6 - Programma Magazzino.xlsx",
+    "\\\\Dl360\\pubbliche\\SCAMBIO DOCUMENTI\\USCITE CAMION_FURGONE\\PROGRAMMA SETTIMANALE CONSEGNE.xlsx"
 ];
 
-const buttons = ["openTecnico", "openOfficina", "openStampaggio", "openTranceria", "openTorneria", "openMagazzino"];
+const buttons = ["openTecnico", "openOfficina", "openStampaggio", "openTranceria", "openTorneria", "openMagazzino", "openConsegne"];
 
 buttons.forEach((id, index) => {
     document.getElementById(id).addEventListener("click", () => {
