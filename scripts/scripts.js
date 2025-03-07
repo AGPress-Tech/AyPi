e2c3deeae417da7f4bff961da61c03e7b93ecf6a
AGPress-Tech <tech@agpress-srl.it>
2025-03-07 08:30:39 +0100

    graphic fix and sliding nicely

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,12 +26,9 @@ document.getElementById("githubIcon").addEventListener("click", () => {
 function openNav() {
     document.getElementById("main").style.marginLeft = "35%";
     document.getElementById("mySidebar").style.width = "35%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
+    document.getElementById("mySidebar").style.width = "0%";
 }
