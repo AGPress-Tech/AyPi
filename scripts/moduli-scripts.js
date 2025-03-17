263fbf3a4253b2023f1ada84b9613b6ca0ee04f5
AGPress-Tech <tech@agpress-srl.it>
2025-03-17 16:38:12 +0100

    Tried to optimized but failed, for now

--- scripts/moduli-scripts.js
+++ scripts/moduli-scripts.js
@@ -36,11 +36,11 @@ function closeNav() {
 }
 
 function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    document.getElementById('clock').textContent = `${hours}:${minutes}`;
-}
-
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        document.getElementById('clock').textContent = `${hours}:${minutes}`;
+    }
+    
 setInterval(updateClock, 1000);
 updateClock();
