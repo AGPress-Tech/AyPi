bff515b2e3630993107e4fa298084947d1f56944
AGPress-Tech <tech@agpress-srl.it>
2025-04-01 11:28:51 +0200

    Added "Strumenti di Misura"

--- scripts/moduli-scripts.js
+++ scripts/moduli-scripts.js
@@ -8,13 +8,14 @@ const filePaths = [
     "\\\\Dl360\\pubbliche\\TECH\\In Edit\\AyPi Valutazione Fornitori\\AyPi - Valutazione Fornitori.accdb",
     "\\\\Dl360\\pubbliche\\MAGAZZINO\\DDT FORNITORI\\Controllo DDT fornitori.accdb",
     "\\\\Dl360\\pubbliche\\QUALITA'\\MANUTENZIONI MACCHINE\\AyPi - Manutenzione Macchine.accdb",
-    "\\\\Dl360\\pubbliche\\TECH\\In Edit\\AyPi Gestione Utensili e Attrezzature\\AyPi - Gestione Utensili e Attrezzature.accdb", 
+    "\\\\Dl360\\pubbliche\\QUALITA'\\CERTIFICAZIONE ISO 9001-2015\\STRUMENTI E TARATURE\\AyPi - Strumenti e Tarature.accdb",
     "\\\\Dl360\\pubbliche\\TECNICO\\MODULO STAMPI\\S1 - Scheda Montaggio Stampi.xlsm",   
     "\\\\Dl360\\pubbliche\\OFF. MECCANICA\\Gestione Morsetti\\AyPi - Gestione Morsetti.accdb",
+    "\\\\Dl360\\pubbliche\\TECH\\In Edit\\AyPi Gestione Utensili e Attrezzature\\AyPi - Gestione Utensili e Attrezzature.accdb",
     "\\\\Dl360\\pubbliche\\TECH\\In Edit\\AyPi Ticket Support\\AyPi - Ticket Support.accdb"
 ];
 
-const buttons = ["openFornitori", "openDDT", "openManutenzioni", "openUtensili", "openModuloStampi", "openMorsetti", "openTicket"];
+const buttons = ["openFornitori", "openDDT", "openManutenzioni", "openTarature", "openModuloStampi", "openMorsetti", "openUtensili", "openTicket"];
 
 buttons.forEach((id, index) => {
     document.getElementById(id).addEventListener("click", () => {
