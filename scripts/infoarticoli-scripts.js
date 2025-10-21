4cfe47a2687ce9573d8f261ae8c3744c87f5654d
Ayrton Pizzi <tech@agpress-srl.it>
2025-10-21 08:18:51 +0200

    Added Scheda Difetti and Scheda Montaggio Stampi

--- scripts/infoarticoli-scripts.js
+++ scripts/infoarticoli-scripts.js
@@ -5,10 +5,12 @@ initCommonUI();
 
 const filePaths = [
     "\\\\Dl360\\pubbliche\\TECNICO\\PROGETTAZIONE\\A.G.PRESS TORNITI\\A.G.PRESS DISEGNI TORNITI",
-    "\\\\Dl360\\pubbliche\\TECNICO\\QUALITA' E MODULISTICA\\DOCUMENTI CONDIVISI A.G.PRESS\\CICLI DI LAVORAZIONE"
+    "\\\\Dl360\\pubbliche\\TECNICO\\QUALITA' E MODULISTICA\\DOCUMENTI CONDIVISI A.G.PRESS\\CICLI DI LAVORAZIONE",
+    "\\\\Dl360\\pubbliche\\TECNICO\\QUALITA' E MODULISTICA\\DOCUMENTI CONDIVISI A.G.PRESS\\SCHEDE MONTAGGIO STAMPI M10-7",
+    "\\\\Dl360\\pubbliche\\TECNICO\\QUALITA' E MODULISTICA\\DOCUMENTI CONDIVISI A.G.PRESS\\SCHEDE DIFETTI DI PRODUZIONE M06-8"
 ];
 
-["openTavole", "openCicli"].forEach((id, index) => {
+["openTavole", "openCicli", "openMontaggioStampi", "openDifettiProduzione"].forEach((id, index) => {
     const btn = document.getElementById(id);
     if (btn) {
         btn.addEventListener("click", () => {
